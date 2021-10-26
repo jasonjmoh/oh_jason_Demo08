0e959cd0370a88d6f50dd6b08db4cfb93c474e32
JungMin Oh <joh67@my.bcit.ca>
2021-10-26 12:21:33 -0700

    added firebase login demo08 finished

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,13 +1,12 @@
 // Your web app's Firebase configuration
-var  firebaseConfig = {
-    apiKey: ,
-    authDomain: ",
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId: 
-  };
-  
+var firebaseConfig = {
+  apiKey: "AIzaSyDngKU3FxTIKNrOmnIws5ZE9pjxz6ivUuk",
+  authDomain: "demo8-jason.firebaseapp.com",
+  projectId: "demo8-jason",
+  storageBucket: "demo8-jason.appspot.com",
+  messagingSenderId: "705800571220",
+  appId: "1:705800571220:web:172c08a93408a7e93420c1"
+};
 
-  const app = firebase.initializeApp(firebaseConfig);
-  
+
+const app = firebase.initializeApp(firebaseConfig);
